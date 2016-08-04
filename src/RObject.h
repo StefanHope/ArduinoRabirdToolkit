cabcd3f7a94344b7e21f81cc23b99529acb57706
Hong-She Liang <starofrainnight@gmail.com>
2016-08-04 10:44:05 +0800

    RObject will now printable

--- src/RObject.h
+++ src/RObject.h
@@ -1,8 +1,23 @@
 #ifndef __INCLUDED_CB5671DA425011E6932B00F1F38F93EF
 #define __INCLUDED_CB5671DA425011E6932B00F1F38F93EF
 
-class RObject
+#include "RTypes.h"
+#include <Print.h>
+#include <Printable.h>
+
+class RObject : protected Printable
 {
+public:
+  RObject(): Printable()
+  {
+  }
+
+  virtual ~RObject()
+  {
+  }
+
+  size_t
+  printTo(Print& p) const;
 };
 
 #endif // __INCLUDED_CB5671DA425011E6932B00F1F38F93EF

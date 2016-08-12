e6c5f3a3c5adcc5b66f0d7f505089d5ffcef8386
Hong-She Liang <starofrainnight@gmail.com>
2016-08-12 18:01:26 +0800

    Added basic RStringRef
    Just a draft

---
+++ src/RStringRef.cpp
@@ -0,0 +1,6 @@
+#include "RStringRef.h"
+
+RStringRef::RStringRef()
+{
+
+}
---
+++ src/RStringRef.h
@@ -0,0 +1,12 @@
+#ifndef __INCLUDED_0D4A5BE6605511E683A100F1F38F93EF
+#define __INCLUDED_0D4A5BE6605511E683A100F1F38F93EF
+
+#include "RAbstractStringRef.h"
+
+class RStringRef : public RAbstractStringRef
+{
+public:
+  RStringRef();
+};
+
+#endif // __INCLUDED_0D4A5BE6605511E683A100F1F38F93EF

df45a999d018798e8e92a660c64147ea09794053
Hong-She Liang <starofrainnight@gmail.com>
2016-08-12 18:00:54 +0800

    Completed part of RAbstractStringRef class

--- src/RAbstractStringRef.cpp
+++ src/RAbstractStringRef.cpp
@@ -2,5 +2,105 @@
 
 RAbstractStringRef::RAbstractStringRef()
 {
+}
 
+RAbstractStringRef::~RAbstractStringRef()
+{
+}
+
+unsigned char
+RAbstractStringRef::equals(const RAbstractStringRef &s) const
+{
+  return ((length() == s.length()) && (compareTo(s) == 0));
+}
+
+unsigned char
+RAbstractStringRef::equals(const char *cstr) const
+{
+  return compareTo(cstr) == 0;
+}
+
+unsigned char
+RAbstractStringRef::operator ==(const RAbstractStringRef &rhs) const
+{
+  return equals(rhs);
+}
+
+unsigned char
+RAbstractStringRef::operator ==(const char *cstr) const
+{
+  return equals(cstr);
+}
+
+unsigned char
+RAbstractStringRef::operator !=(const RAbstractStringRef &rhs) const
+{
+  return !equals(rhs);
+}
+
+unsigned char
+RAbstractStringRef::operator !=(const char *cstr) const
+{
+  return !equals(cstr);
+}
+
+unsigned char
+RAbstractStringRef::operator <(const RAbstractStringRef &rhs) const
+{
+  return compareTo(rhs) < 0;
+}
+
+unsigned char
+RAbstractStringRef::operator >(const RAbstractStringRef &rhs) const
+{
+  return compareTo(rhs) > 0;
+}
+
+unsigned char
+RAbstractStringRef::operator <=(const RAbstractStringRef &rhs) const
+{
+  return compareTo(rhs) <= 0;
+}
+
+unsigned char
+RAbstractStringRef::operator >=(const RAbstractStringRef &rhs) const
+{
+  return compareTo(rhs) >= 0;
+}
+
+unsigned char
+RAbstractStringRef::startsWith(const RAbstractStringRef &prefix) const
+{
+  return startsWith(prefix, 0);
+}
+
+char
+RAbstractStringRef::operator [](unsigned int index) const
+{
+  return charAt(index);
+}
+
+void
+RAbstractStringRef::getBytes(unsigned char *buf, unsigned int bufsize,
+                             unsigned int index) const
+{
+  if(!bufsize || !buf)
+  {
+    return;
+  }
+
+  if(index >= length())
+  {
+    buf[0] = 0;
+    return;
+  }
+
+  // FIXME: Not implemented!
+}
+
+void
+RAbstractStringRef::toCharArray(char *buf, unsigned int bufsize,
+                                unsigned int index) const
+{
+  getBytes(reinterpret_cast<unsigned char *>(buf), bufsize, index);
 }
--- src/RAbstractStringRef.h
+++ src/RAbstractStringRef.h
@@ -4,52 +4,84 @@
 class RAbstractStringRef
 {
 public:
-  RAbstractStringRef()
-  {
-  }
+  RAbstractStringRef();
 
-  virtual ~RAbstractStringRef()
-  {
-  }
+  virtual
+  ~RAbstractStringRef();
 
-//  virtual unsigned int length(void) const = 0;
-//  virtual int compareTo(const RAbstractStringRef &s) const = 0;
-//  virtual unsigned char equals(const RAbstractStringRef &s) const = 0;
-//  virtual unsigned char equals(const char *cstr) const = 0;
-//  unsigned char operator == (const RAbstractStringRef &rhs) const {return equals(rhs);} = 0;
-//  unsigned char operator == (const char *cstr) const {return equals(cstr);}
-//  unsigned char operator != (const RAbstractStringRef &rhs) const {return !equals(rhs);}
-//  unsigned char operator != (const char *cstr) const {return !equals(cstr);}
-//  virtual unsigned char operator <  (const RAbstractStringRef &rhs) const;
-//  virtual unsigned char operator >  (const RAbstractStringRef &rhs) const;
-//  virtual unsigned char operator <= (const RAbstractStringRef &rhs) const;
-//  virtual unsigned char operator >= (const RAbstractStringRef &rhs) const;
-//  virtual unsigned char equalsIgnoreCase(const RAbstractStringRef &s) const;
-//  virtual unsigned char startsWith( const RAbstractStringRef &prefix) const;
-//  virtual unsigned char startsWith(const RAbstractStringRef &prefix, unsigned int offset) const;
-//  virtual unsigned char endsWith(const RAbstractStringRef &suffix) const;
+  virtual unsigned int
+  length(void) const = 0;
+  virtual int
+  compareTo(const RAbstractStringRef &s) const = 0;
+  virtual int
+  compareTo(const char *s) const = 0;
+  unsigned char
+  equals(const RAbstractStringRef &s) const;
+  virtual unsigned char
+  equals(const char *cstr) const;
+  unsigned char
+  operator ==(const RAbstractStringRef &rhs) const;
+  unsigned char
+  operator ==(const char *cstr) const;
+  unsigned char
+  operator !=(const RAbstractStringRef &rhs) const;
+  unsigned char
+  operator !=(const char *cstr) const;
+  unsigned char
+  operator <(const RAbstractStringRef &rhs) const;
+  unsigned char
+  operator >(const RAbstractStringRef &rhs) const;
+  unsigned char
+  operator <=(const RAbstractStringRef &rhs) const;
+  unsigned char
+  operator >=(const RAbstractStringRef &rhs) const;
+  virtual unsigned char
+  equalsIgnoreCase(const RAbstractStringRef &s) const = 0;
+  unsigned char
+  startsWith(const RAbstractStringRef &prefix) const;
+  virtual unsigned char
+  startsWith(const RAbstractStringRef &prefix, unsigned int offset) const = 0;
+  virtual unsigned char
+  endsWith(const RAbstractStringRef &suffix) const = 0;
 
-//  // character acccess
-//  char charAt(unsigned int index) const;
-//  void setCharAt(unsigned int index, char c);
-//  char operator [] (unsigned int index) const;
-//  char& operator [] (unsigned int index);
-//  void getBytes(unsigned char *buf, unsigned int bufsize, unsigned int index=0) const;
-//  void toCharArray(char *buf, unsigned int bufsize, unsigned int index=0) const
-//    {getBytes((unsigned char *)buf, bufsize, index);}
-//  const char * c_str() const { return buffer; }
-//  const char* begin() { return c_str(); }
-//  const char* end() { return c_str() + length(); }
+  // character acccess
+  virtual char
+  charAt(unsigned int index) const = 0;
 
-//  // search
-//  int indexOf( char ch ) const;
-//  int indexOf( char ch, unsigned int fromIndex ) const;
-//  int indexOf( const RAbstractStringRef &str ) const;
-//  int indexOf( const RAbstractStringRef &str, unsigned int fromIndex ) const;
-//  int lastIndexOf( char ch ) const;
-//  int lastIndexOf( char ch, unsigned int fromIndex ) const;
-//  int lastIndexOf( const RAbstractStringRef &str ) const;
-//  int lastIndexOf( const RAbstractStringRef &str, unsigned int fromIndex ) const;
+  char
+  operator [](unsigned int index) const;
+
+  virtual void
+  getBytes(unsigned char *buf, unsigned int bufsize,
+           unsigned int index=0) const;
+
+  void
+  toCharArray(char *buf, unsigned int bufsize, unsigned int index=0) const;
+
+  // search
+  int
+  indexOf(char ch) const;
+
+  virtual int
+  indexOf(char ch, unsigned int fromIndex) const;
+
+  int
+  indexOf(const RAbstractStringRef &str) const;
+
+  virtual int
+  indexOf(const RAbstractStringRef &str, unsigned int fromIndex) const;
+
+  int
+  lastIndexOf(char ch) const;
+
+  virtual int
+  lastIndexOf(char ch, unsigned int fromIndex) const;
+
+  int
+  lastIndexOf(const RAbstractStringRef &str) const;
+
+  virtual int
+  lastIndexOf(const RAbstractStringRef &str, unsigned int fromIndex) const;
 };
 
 #endif // _INCLUDED_CEFD8F1A5D4C11E68E7800F1F38F93EF

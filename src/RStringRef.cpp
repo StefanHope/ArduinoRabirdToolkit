183eaf0cb87336e373315b21925ddecad04050cb
Hong-She Liang <starofrainnight@gmail.com>
2016-08-14 19:25:32 +0800

    Completed RStringRef

--- src/RStringRef.cpp
+++ src/RStringRef.cpp
@@ -1,6 +1,35 @@
 #include "RStringRef.h"
 
 RStringRef::RStringRef()
+  : mStr(NULL)
 {
+}
 
+RStringRef::RStringRef(const String *str)
+  : mStr(str)
+{
+}
+
+unsigned int
+RStringRef::length() const
+{
+  if(mStr)
+  {
+    return mStr->length();
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+char
+RStringRef::charAt(unsigned int index) const
+{
+  if((NULL == mStr) || (index >= length()))
+  {
+    return '\0';
+  }
+
+  return mStr->charAt(index);
 }
--- src/RStringRef.h
+++ src/RStringRef.h
@@ -2,11 +2,23 @@
 #define __INCLUDED_0D4A5BE6605511E683A100F1F38F93EF
 
 #include "RAbstractStringRef.h"
+#include "WString.h"
 
 class RStringRef : public RAbstractStringRef
 {
 public:
   RStringRef();
+  RStringRef(const String *str);
+
+  // RAbstractStringRef interfaces:
+  unsigned int
+  length() const;
+
+  char
+  charAt(unsigned int index) const;
+
+private:
+  const String * mStr;
 };
 
 #endif // __INCLUDED_0D4A5BE6605511E683A100F1F38F93EF

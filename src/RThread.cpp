3553679ab56677ff3317a20db385c8839c5727ec
Hong-She Liang <starofrainnight@gmail.com>
2016-09-22 02:33:48 +0800

    Added RThread class

---
+++ src/RThread.cpp
@@ -0,0 +1,206 @@
+#include "RThread.h"
+
+class RThreadPrivate
+{
+public:
+  static void
+  run(void *arg);
+};
+
+void
+RThreadPrivate::run(void *arg)
+{
+  RThread *thread = static_cast<RThread *>(arg);
+
+  if(thread)
+  {
+    thread->started.emit();
+    thread->run();
+    thread->finished.emit();
+  }
+}
+
+RThread::RThread(TaskHandle_t handle) : RThread()
+{
+  mHandle = handle;
+}
+
+RThread::RThread()
+  : mStackSize(128)
+  , mHandle(NULL)
+  , mIsOwnded(false)
+{
+}
+
+RThread::~RThread()
+{
+  terminate();
+}
+
+void
+RThread::exit(int returnCode)
+{
+  // FIXME: We should post a message to this thread's queue just tell the thread
+  // with specific exit code. Now just simple terminate() this thread (very
+  // dangerous)
+  terminate();
+}
+
+bool
+RThread::isFinished() const
+{
+  if(!mHandle)
+  {
+    return true;
+  }
+
+  return (eDeleted == eTaskGetState(mHandle));
+}
+
+bool
+RThread::isRunning() const
+{
+  if(!mHandle)
+  {
+    return false;
+  }
+
+  return (eDeleted != eTaskGetState(mHandle));
+}
+
+RThread::Priority
+RThread::priority() const
+{
+  if(!mHandle)
+  {
+    return IdlePriority;
+  }
+
+  return static_cast<RThread::Priority>(uxTaskPriorityGet(mHandle));
+}
+
+void
+RThread::setPriority(RThread::Priority priority)
+{
+  if(!mHandle)
+  {
+    return;
+  }
+
+  vTaskPrioritySet(mHandle, static_cast<UBaseType_t>(priority));
+}
+
+void
+RThread::setStackSize(size_t stackSize)
+{
+  mStackSize = stackSize;
+}
+
+bool
+RThread::wait(unsigned long time)
+{
+  /// FIXME: Ugly way to wait for thread finished.
+
+  /* Block for 100ms each time */
+  const TickType_t ticksPerBlock = 100 / portTICK_PERIOD_MS;
+
+  if(currentThreadId() == mHandle)
+  {
+    // You can't wait yourself in the samethread!
+    return false;
+  }
+
+  while(true)
+  {
+    if(isFinished())
+    {
+      return true;
+    }
+
+    if(time < std::numeric_limits<unsigned long>::max())
+    {
+      if(time <= 100)
+      {
+        // Timeout!
+        return false;
+      }
+      else
+      {
+        time -= 100;
+      }
+    }
+
+    vTaskDelay(ticksPerBlock);
+  }
+}
+
+void
+RThread::quit()
+{
+  this->exit(0);
+}
+
+void
+RThread::start(RThread::Priority priority)
+{
+  /* Create the task, storing the handle. */
+  TaskHandle_t handle = NULL;
+
+  if(isRunning())
+  {
+    return;
+  }
+
+  terminate();
+
+  UBaseType_t targetPriority = IdlePriority;
+
+  if(InheritPriority == priority)
+  {
+    targetPriority = uxTaskPriorityGet(currentThreadId());
+  }
+
+  auto ret = xTaskCreate(
+    RThreadPrivate::run,  /* Function that implements the task. */
+    "",                /* Text name for the task. */
+    (stackSize() + 1) % sizeof(uint16_t), /* Stack size in words, not bytes. */
+    static_cast<void *>(this),      /* Parameter passed into the task. */
+    targetPriority,  /* Priority at which the task is created. */
+    &handle);   /* Used to pass out the created task's handle. */
+
+  if(ret == pdPASS)
+  {
+    mIsOwnded = true;
+    mHandle   = handle;
+  }
+}
+
+void
+RThread::terminate()
+{
+  if(mIsOwnded && mHandle)
+  {
+    vTaskDelete(mHandle);
+    terminated.emit();
+  }
+
+  mHandle = NULL;
+}
+
+TaskHandle_t
+RThread::currentThreadId()
+{
+  return xTaskGetCurrentTaskHandle();
+}
+
+void
+RThread::yieldCurrentThread()
+{
+  taskYIELD();
+}
+
+void
+RThread::run()
+{
+  // TODO: We sould run event loop here!
+}
---
+++ src/RThread.h
@@ -0,0 +1,100 @@
+#ifndef __INCLUDED_458662EC691611E6AA6E0021CC66E280
+#define __INCLUDED_458662EC691611E6AA6E0021CC66E280
+
+#include "RObject.h"
+#include "RSignal.h"
+#include <Arduino_FreeRTOS.h>
+#include <limits>
+
+class RThreadPrivate;
+class RThread : public RObject
+{
+public:
+  /**
+   * This enum type indicates how the operating system should schedule newly
+   * created threads.
+   */
+  enum Priority
+  {
+    /// scheduled only when no other threads are running.
+    IdlePriority /* = tskIDLE_PRIORITY */,
+    /// scheduled less often than LowPriority.
+    LowestPriority,
+    /// scheduled less often than NormalPriority.
+    LowPriority,
+    /// the default priority of the operating system.
+    NormalPriority,
+    /// scheduled more often than NormalPriority.
+    HighPriority,
+    /// scheduled more often than HighPriority.
+    HighestPriority,
+    /// scheduled as often as possible.
+    TimeCriticalPriority,
+    /// use the same priority as the creating thread. This is the default.
+    InheritPriority,
+  };
+
+private:
+  RThread(TaskHandle_t handle);
+
+public:
+  RThread();
+
+  virtual
+  ~RThread();
+
+  /**
+   * Tells the thread's event loop to exit with a return code.
+   *
+   * @brief exit
+   * @param returnCode
+   * @note Not implemented
+   */
+  void
+  exit(int returnCode=0);
+  bool
+  isFinished() const;
+  bool
+  isRunning() const;
+  Priority
+  priority() const;
+  void
+  setPriority(Priority priority);
+  void
+  setStackSize(size_t stackSize);
+  size_t
+  stackSize() const;
+  bool
+  wait(unsigned long time=std::numeric_limits<unsigned long>::max());
+
+  void
+  quit();
+  void
+  start(Priority priority=InheritPriority);
+  void
+  terminate();
+
+  static TaskHandle_t
+  currentThreadId();
+  static void
+  yieldCurrentThread();
+
+  RSignal<void()> started;
+  RSignal<void()> finished;
+  RSignal<void()> terminated;
+
+protected:
+  virtual void
+  run();
+
+private:
+  size_t       mStackSize;
+  TaskHandle_t mHandle;
+  bool         mIsOwnded;
+
+  friend RThreadPrivate;
+
+  R_DISABLE_COPY(RThread)
+};
+
+#endif // __INCLUDED_458662EC691611E6AA6E0021CC66E280

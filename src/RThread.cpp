8c444a81a57df2ddf3183eab2211639baa4c91d0
Hong-She Liang <starofrainnight@gmail.com>
2017-01-12 15:49:52 +0800

    Fixed task not running
    Give stack size a wrong value

--- src/RThread.cpp
+++ src/RThread.cpp
@@ -183,7 +183,7 @@ RThread::start(RThread::Priority priority)
   auto ret = xTaskCreate(
     RThreadPrivate::run,  /* Function that implements the task. */
     "",                /* Text name for the task. */
-    (stackSize() + 1) % sizeof(uint16_t), /* Stack size in words, not bytes. */
+    (stackSize() + 1) / sizeof(uint16_t), /* Stack size in words, not bytes. */
     static_cast<void *>(this),      /* Parameter passed into the task. */
     targetPriority,  /* Priority at which the task is created. */
     &handle);   /* Used to pass out the created task's handle. */
